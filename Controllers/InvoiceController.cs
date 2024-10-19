dee216ceba02a3aa952695a951093472c2ba3880
mehmetiyiyazici <164754796+mehmetiyiyazici@users.noreply.github.com>
2024-10-19 14:57:54 +0300

    Modeller düzenlendir controller yazılıdı.

--- Controllers/InvoiceController.cs
+++ Controllers/InvoiceController.cs
@@ -1,4 +1,5 @@
 using InvoiceApp.Data;
+using InvoiceApp.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InvoiceApp.Controllers;
@@ -11,5 +12,64 @@ public class InvoiceController : Controller
     public InvoiceController(AppDbContext context)
     {
         _context = context;
+    }
+    
+    public List<Invoice> GetInvoices()
+    {
+        var ınvoice = _context.Invoices.ToList();
+        return ınvoice;
+    }
+
+    // [HttpGet]
+    // [Route("[id]")]
+    // yukarıdaki iki satır yerine bunları birleştirip tek satırda yazabiliyoruz.
+    [HttpGet("{id}")]
+    public Invoice GetInvoice(int id)
+    {
+        var lesson = _context.Invoices.Find(id);
+        if (lesson is null) 
+            return new Invoice(); // NotFound()
+        return lesson; // Ok(lesson)
+    }
+
+    [HttpPost]
+    public IActionResult SaveInvoice([FromBody] Invoice model)
+    {
+        
+            _context.Invoices.Add(model); 
+            _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetInvoice), new { id = model.Id }, model);
+       
+    }
+    public IActionResult UpdateInvoice([FromBody] Invoice model)
+    {
+        model = _context.Invoices.Find(model.Id);
+        model.InvoiceName = model.InvoiceName;
+        model.Description = model.Description;
+        model.PaymentStatus = model.PaymentStatus;
+        model.User = model.User;
+        _context.Invoices.Update(model); 
+        _context.SaveChangesAsync();
+        return Ok("Başarılıyla kaydedildi.");
+       
+    }
+
+
+    [HttpDelete("{id}")]
+    public string DeleteInvoice(int id)
+    {
+        try
+        {
+            var data = _context.Invoices.Find(id);
+            _context.Remove(data);
+            _context.SaveChanges();
+            return "Başarıyla silindi";
+        }
+        catch (Exception e)
+        {
+            return  "Silme işlemi sırıasın bir hata oluştu.\n" + e.Message;
+        }
+        
+        
     }
 }
--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -7,6 +7,8 @@ public class AppDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
     public DbSet<Invoice> Invoices { get; set; }
+    
+    public DbSet<Item> Items { get; set; }
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
         
