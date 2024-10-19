d2cabbef9b9233ad4e4335eb722130cd9804adac
Baris <bariscakdi@gmail.com>
2024-10-19 12:57:27 +0300

    context düzenleme

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -4,11 +4,6 @@ namespace InvoiceApp.Data;
 
 public class AppDbContext : DbContext
 {
-    // public DbSet<Teacher> Teachers { get; set; }
-    // public DbSet<User> Users { get; set; }
-    // public DbSet<ProgramModel> Programs { get; set; }
-    // public DbSet<ManyToManyUserProgram> ManyToManyUserPrograms { get; set; }
-    // public DbSet<History> Histories { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
