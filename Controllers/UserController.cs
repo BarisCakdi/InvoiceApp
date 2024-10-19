99709eac9dadf9c22d3065d2f04ad40ecef755a3
Baris <bariscakdi@gmail.com>
2024-10-19 13:47:51 +0300

    ClientController add

---
+++ Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using InvoiceApp.Data;
+using InvoiceApp.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace InvoiceApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context) 
+        {
+            _context = context; 
+        }
+        [HttpGet]
+        public List<User> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        [HttpPost]
+        public User SaveUser([FromBody] User model)
+        {
+            if (model != null)
+            {
+                _context.Users.Update(model);
+            }
+            else
+            {
+                _context.Users.Add(model);
+            }
+            _context.SaveChanges();
+            return model;
+        }
+    }
+}
